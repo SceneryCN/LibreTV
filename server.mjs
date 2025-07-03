073f80a4c6df7eb2125f693dad051db7be3d6358
Normam <cnormannes@gmail.com>
2025-07-03 18:54:54 +0800

    fix(a): fixAboutStyle

--- server.mjs
+++ server.mjs
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 
 const config = {
   port: process.env.PORT || 8080,
-  password: process.env.PASSWORD || '',
+  password:  '' || process.env.PASSWORD,
   adminpassword: process.env.ADMINPASSWORD || '',
   corsOrigin: process.env.CORS_ORIGIN || '*',
   timeout: parseInt(process.env.REQUEST_TIMEOUT || '5000'),
